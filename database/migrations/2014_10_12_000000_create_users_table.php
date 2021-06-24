1c1236b9141ba3dd1c03a061d4ebb383efb542de
Cita.17 <citaaji@student.ub.ac.id>
2021-06-24 10:51:21 +0700

    database 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 250)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->integer('role');
+            $table->text('profil_image_url')->nullable();
+            $table->text('background_image_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
