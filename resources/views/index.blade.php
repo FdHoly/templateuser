72b4f8ec1041e9ace2b0625e3dbbff0349b8f037
Cita.17 <citaaji@student.ub.ac.id>
2021-06-28 08:38:47 +0700

    commit laporan dari Cita

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1213,7 +1213,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">Laporkan Jawaban</h5>
+                        <h5 class="modal-title" id="exampleModalLongTitle">Laporkan Post</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
