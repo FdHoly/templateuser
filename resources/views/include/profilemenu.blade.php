b46c3de2fee2ac347d12cd1b22ac1b219a1c3362
azzamhfa <azzamuddienhanifa@gmail.com>
2021-06-30 13:34:06 +0700

    Finish ???? idk lmao

--- resources/views/include/profilemenu.blade.php
+++ resources/views/include/profilemenu.blade.php
@@ -6,6 +6,7 @@
                     <li class="{{Route::currentRouteName() == 'profile' ? 'active' : ''}}"><a href="{{ route('profile') }}">Post</a></li>
                     <li class="{{Route::currentRouteName() == 'friends' ? 'active' : ''}}"><a href="{{ route('friends')}}">Organisasi</a></li>
                     <li class="{{Route::currentRouteName() == 'pengumuman' ? 'active' : ''}}"><a href="{{ route('pengumuman')}}">Pengumuman</a></li>
+                    <li class="{{Route::currentRouteName() == 'laporan' ? 'active' : ''}}"><a href="{{ route('laporan')}}">Laporan</a></li>
                 </ul>
             </nav>
         </div>
